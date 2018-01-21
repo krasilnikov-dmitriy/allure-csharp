d9a4bc706ecbd3379a604fff63895ef8de7643e7
Krasilnikov Dmitry <krasilnikov.dmitriy@gmail.com>
2018-01-21 22:56:29 +0300

    Add Xunit reporter

---
+++ Allure.XUnitPlugin/AllureReporter.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace Allure.XUnitPlugin
+{
+    public class AllureReporter : IRunnerReporter
+    {
+        public string Description => "Reporting tests results to Allure";
+
+        public bool IsEnvironmentallyEnabled => false;
+
+        public string RunnerSwitch => "allure";
+
+        public IMessageSink CreateMessageHandler(IRunnerLogger logger) => new AllureReporterMessageHandler(logger);
+    }
+}
---
+++ Allure.XUnitPlugin/AllureReporterMessageHandler.cs
@@ -0,0 +1,51 @@
+﻿using Allure.Commons;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Allure.XUnitPlugin
+{
+    public class AllureReporterMessageHandler : TestMessageSink
+    {
+        readonly IRunnerLogger logger;
+        static AllureLifecycle allure = AllureLifecycle.Instance;
+
+        public AllureReporterMessageHandler(IRunnerLogger logger)
+        {
+            this.logger = logger;
+
+            Execution.TestCollectionStartingEvent += HandleTestCollectionStarting;
+            Execution.TestCollectionFinishedEvent += HandleTestCollectionFinished;
+
+            /*Execution.TestMethodStartingEvent += HandleMethodStartingEvent;
+            Execution.TestMethodFinishedEvent += HandleMethodFinishedEvent;
+
+            Execution.TestStartingEvent += HandleStarting;
+
+            Execution.TestPassedEvent += HandlePassed;
+            Execution.TestSkippedEvent += HandleSkipped;
+            Execution.TestFailedEvent += HandleFailed;
+
+            Execution.TestFinishedEvent += HandleFinishedEvent;*/
+        }
+        
+        private void HandleTestCollectionStarting(MessageHandlerArgs<ITestCollectionStarting> args)
+        {
+            var testCollectionStarting = args.Message;
+            string key = $"{testCollectionStarting.TestCollection.UniqueID}";
+            
+            var collectionContainer = new TestResultContainer
+            {
+                uuid = key,
+                name = testCollectionStarting.TestCollection.DisplayName
+            };
+            
+            allure.StartTestContainer(collectionContainer);
+        }
+
+        private void HandleTestCollectionFinished(MessageHandlerArgs<ITestCollectionFinished> args)
+        {
+            string key = $"{args.Message.TestCollection.UniqueID}";
+            allure.StopTestContainer(key);
+        }
+    }
+}
